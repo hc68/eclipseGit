ea61ebaa57b658864c49ff61d347985fd91c9361
jack01 <mac_whc@qq.com>
2020-09-12 13:22:20 +0800

    分支dev提交

--- src/main/java/com/game/webmaven/de/hello.java
+++ src/main/java/com/game/webmaven/de/hello.java
@@ -5,6 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Git 在 eclipse中的使用");
 		System.out.println("第一次版本测试");
+		System.out.println("在分支dev中 修改");
 
 	}
 
